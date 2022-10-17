1f8d5957e8c291987c542d9576abc6ac2c830371
Maayane AHAROUNI <75632274+maayaneaha@users.noreply.github.com>
2022-10-17 15:31:41 +0200

    Delete mLsfDemo_0064.bat

--- src/mLsfDemo_0064.bat
+++
@@ -1,3 +0,0 @@
-rem 
-rem python mLsfML3d.py --in ./select/ --mod ./model/ --set directory --nb 9999999999 --play sequence --aug no --naug 1 --dis hands --vec hands,pose --dim 3 --attempt 1 --sample 8 --xpol x --cfg medium --src lsf-xxxx --df lsf-xxxx_3_hp_8.npy --action demo  2>nul
-python mLsfML3d.py --play alea --dis hands --xpol x --src lsf-0064 --df lsf-0064_3_hp_8.npy --action demo 2>nul
